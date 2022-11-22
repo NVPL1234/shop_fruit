754dbc668ce50be2c91dd122e76004043809c044
phongok <phongdevgithub666666@gmail.com>
2022-11-22 22:21:40 +0700

    create project

---
+++ App.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Screen0 from './Screen0'
+import Screen1 from './Screen1'
+import Screen2 from './Screen2'
+import Screen3 from './Screen3'
+import Context from './Context'
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+    return (
+      <Context>
+          <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Screen0' component={Screen0} />
+                {/* <Stack.Screen name='Screen1' component={Screen1} />
+                <Stack.Screen name='Screen2' component={Screen2} />
+                <Stack.Screen name='Screen3' component={Screen3} /> */}
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Context>
+    )
+}
+
+export default App
---
+++ Screen0.js
@@ -0,0 +1,25 @@
+import React, { useContext, createContext, useEffect } from 'react'
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+
+
+
+export default function Screen0() {
+
+
+  return (
+    <View style={styles.container}>
+      <Text>kkk</Text>
+      <Button title='kk'/>
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: '5%',
+    padding: '1%'
+  },
+});
