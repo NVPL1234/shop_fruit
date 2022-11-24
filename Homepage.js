f1109b62811756d672a5962325c15c22784f1f8e
Phan Tấn Tài <taito1doraemon@gmail.com>
2022-11-24 11:37:56 +0700

    finish homepagescreen

--- Homepage.js
+++ Homepage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge, Box, Center, HStack, Icon, IconButton, Image, Input, NativeBaseProvider, Text } from 'native-base';
+import { Badge, Box, Center, HStack, Icon, IconButton, Image, Input, NativeBaseProvider, Pressable, Text } from 'native-base';
 import { FontAwesome } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import banner from './images/24606e32a04a52140b5b_1617076998.jpg';
@@ -8,9 +8,16 @@ import apple from './assets/apple.png';
 import apples from './assets/apples.png';
 import { MyData } from './Context';
 
-export default function Homepage() {
+export default function Homepage({ navigation }) {
 
   const { fruits } = React.useContext(MyData);
+  const popularFruits = fruits.sort((a,b) => a.rating - b.rating);
+  const suggestFruits = fruits.sort((a,b) => b.sale - a.sale);
+
+  const onClickItem = (item) => {
+    console.log('data: ', item);
+    navigation.navigate("Detailpage", {item: item});
+  }
 
   return (
     <NativeBaseProvider accessible={true}>
@@ -37,56 +44,16 @@ export default function Homepage() {
               <HStack>
                   {
                     fruits.map(o => {
-                        return <Center mx="1" key={Math.random()}>
-                                  <Box bg="white" p="3" borderRadius="10">
-                                      <Image source={o.image} alt="apple" size={45} borderRadius={150} />
-                                  </Box>
-                                  <Text>{o.name}</Text>
-                              </Center>
+                        return <Pressable key={Math.random()} onPress={() => onClickItem(o)}>
+                                    <Center mx="1">
+                                        <Box bg="white" p="3" borderRadius="10">
+                                            <Image source={o.image} alt="apple" size={45} borderRadius={150} />
+                                        </Box>
+                                        <Text>{o.name}</Text>
+                                    </Center>
+                                </Pressable>
                     })
                   }
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                  </Center>
               </HStack>
           </Box>
 
@@ -99,55 +66,20 @@ export default function Homepage() {
           </Box>
           <Box>
               <HStack>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text>$12</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text>$12</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text>$12</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text>$12</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text>$12</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text>$12</Text>
-                  </Center>
-                  <Center mx="1">
-                      <Box bg="white" p="3" borderRadius="10">
-                          <Image source={apple} alt="apple" size={45} borderRadius={150} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text>$12</Text>
-                  </Center>
+                  {
+                    popularFruits.map(o => {
+                        return <Pressable key={Math.random()} onPress={() => onClickItem(o)}>
+                                <Center mx="1">
+                                    <Box bg="white" p="3" borderRadius="10">
+                                        <Image source={o.image} alt="apple" size={45} borderRadius={150} />
+                                    </Box>
+                                    <Text>{o.name}</Text>
+                                    <Text fontWeight="bold">${o.price}.0</Text>
+                                    <Text>{o.rating} ⭐</Text>
+                                </Center>
+                            </Pressable>
+                    })
+                  }
               </HStack>
           </Box>
 
@@ -160,51 +92,21 @@ export default function Homepage() {
           </Box>
           <Box>
               <HStack alignItems="center">
-                  <Center>
-                      <Box bg="white" borderRadius="15" borderWidth="1" borderColor="blue.200">
-                          <Badge colorScheme="success">-17% OFF</Badge>
-                          <Image source={apples} alt="apples" size={90} borderRadius={20} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text strikeThrough color="error.400">$120.0</Text>
-                      <Text fontWeight="bold" color="success.400">$100.0</Text>
-                  </Center>
-                  <Center>
-                      <Box bg="white" borderRadius="15" borderWidth="1" borderColor="blue.200">
-                          <Badge colorScheme="success">-17% OFF</Badge>
-                          <Image source={apples} alt="apples" size={90} borderRadius={20} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text strikeThrough color="error.400">$120.0</Text>
-                      <Text fontWeight="bold" color="success.400">$100.0</Text>
-                  </Center>
-                  <Center>
-                      <Box bg="white" borderRadius="15" borderWidth="1" borderColor="blue.200">
-                          <Badge colorScheme="success">-17% OFF</Badge>
-                          <Image source={apples} alt="apples" size={90} borderRadius={20} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text strikeThrough color="error.400">$120.0</Text>
-                      <Text fontWeight="bold" color="success.400">$100.0</Text>
-                  </Center>
-                  <Center>
-                      <Box bg="white" borderRadius="15" borderWidth="1" borderColor="blue.200">
-                          <Badge colorScheme="success">-17% OFF</Badge>
-                          <Image source={apples} alt="apples" size={90} borderRadius={20} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text strikeThrough color="error.400">$120.0</Text>
-                      <Text fontWeight="bold" color="success.400">$100.0</Text>
-                  </Center>
-                  <Center>
-                      <Box bg="white" borderRadius="15" borderWidth="1" borderColor="blue.200">
-                          <Badge colorScheme="success">-17% OFF</Badge>
-                          <Image source={require('./assets/bananas.png')} alt="apples" size={90} borderRadius={20} />
-                      </Box>
-                      <Text>Apples</Text>
-                      <Text strikeThrough color="error.400">$120.0</Text>
-                      <Text fontWeight="bold" color="success.400">$100.0</Text>
-                  </Center>
+                  {
+                    suggestFruits.map(o => {
+                        return <Pressable key={Math.random()} onPress={() => onClickItem(o)}>
+                                    <Center>
+                                        <Box bg="white" borderRadius="15" borderWidth="1" borderColor="blue.200">
+                                            <Badge colorScheme="success">{o.sale}% OFF</Badge>
+                                            <Image source={o.image} alt="apples" size={90} borderRadius={20} />
+                                        </Box>
+                                        <Text>{o.name}</Text>
+                                        <Text strikeThrough color="error.400">${o.price}.0</Text>
+                                        <Text fontWeight="bold" color="success.400">${o.price}.0</Text>
+                                    </Center>
+                                </Pressable>
+                    })
+                  }
               </HStack>
           </Box>
       </Box>
